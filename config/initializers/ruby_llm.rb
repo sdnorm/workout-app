9cf04a9b9441189dd27eaf8f48655c4347a03e59
Spencer Norman <spencerdnorman@gmail.com>
2026-02-13 18:21:11 -0600

    switched up how the rubyllm initializer sees the keys

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,6 +1,6 @@
 RubyLLM.configure do |config|
-  config.anthropic_api_key = ENV["ANTHROPIC_API_KEY"]
-  config.xai_api_key = ENV["XAI_API_KEY"]
+  config.anthropic_api_key = Rails.application.credentials.dig(:anthropic_api_key) || ENV["ANTHROPIC_API_KEY"]
+  config.xai_api_key = Rails.application.credentials.dig(:xai_api_key) || ENV["XAI_API_KEY"]
 
   config.use_new_acts_as = true
 end
